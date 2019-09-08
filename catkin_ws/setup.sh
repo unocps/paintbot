4ae7cb05063e9520300c4954d13ccc8e8e63410f
Ryan Lankin <rlankin@unomaha.edu>
2019-09-08 18:07:08 -0500

    Renamed bricklayer to paintbot

--- catkin_ws/setup.sh
+++ catkin_ws/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 models_dir=$(dirname $(realpath setup.sh))
-echo 'export GAZEBO_MODEL_PATH='$models_dir'/src/bricklayer_gazebo/models/:$GAZEBO_MODEL_PATH' >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH='$models_dir'/src/paintbot_gazebo/models/:$GAZEBO_MODEL_PATH' >> ~/.bashrc
