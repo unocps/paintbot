749d921e20896d72f2b344c06d8188384c4232b3
Ryan Lankin <rlankin@unomaha.edu>
2020-01-18 19:09:44 -0600

    Removed navigation node; Planning node now sends navigation goals directly

--- catkin_ws/src/paintbot_description/src/paintbot/planning.py
+++ catkin_ws/src/paintbot_description/src/paintbot/planning.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-from gazebo_msgs.msg import ModelStates
 from lib import constants, util
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from paintbot_description.msg import PaintTarget
 from tf.transformations import quaternion_from_euler
+import actionlib
 import enum
 import geometry_msgs
 import math
@@ -14,10 +15,10 @@ ACTION_DIST = 0.5
 WALL_THICKNESS = 0.05
 
 class Task:
-    def __init__(self, action, dest, orient):
+    def __init__(self, action, dest, orientation):
         self.action = action
         self.dest = dest
-        self.orient = orient
+        self.orientation = orientation
 
 State = enum.Enum('State', 'NAVIGATE ACTION')
 
@@ -27,8 +28,26 @@ tasks = [
 ]
 t_i = 0
 st = State.NAVIGATE
+move_base_client = None
 paint_pub = None
-nav_pub = None
+
+def navigate(x, y, yaw):
+    goal_x = x + ((ACTION_DIST + WALL_THICKNESS) * math.cos(yaw))
+    goal_y = y + ((ACTION_DIST + WALL_THICKNESS) * math.sin(yaw))
+    goal_yaw = util.normalize_angle(yaw - math.pi)
+
+    rospy.loginfo('Navigating to ({:.2f}, {:.2f}) @ {:.2f}'.format(goal_x, goal_y, goal_yaw))
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = 'map'
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = goal_x
+    goal.target_pose.pose.position.y = goal_y
+    goal.target_pose.pose.orientation = geometry_msgs.msg.Quaternion(*quaternion_from_euler(0, 0, goal_yaw))
+
+    move_base_client.send_goal(goal)
+    wait = move_base_client.wait_for_result()
+    result = move_base_client.get_result()
 
 def handle_notification(msg):
     global t_i, st
@@ -37,8 +56,8 @@ def handle_notification(msg):
         task = tasks[t_i]
         st = State.ACTION
         msg = PaintTarget()
-        msg.x = task.dest[0] + (WALL_THICKNESS * math.cos(task.orient))
-        msg.y = task.dest[1] + (WALL_THICKNESS * math.sin(task.orient))
+        msg.x = task.dest[0] + (WALL_THICKNESS * math.cos(task.orientation))
+        msg.y = task.dest[1] + (WALL_THICKNESS * math.sin(task.orientation))
         msg.action = task.action
 
         rospy.sleep(1) # TODO: Debug
@@ -49,33 +68,30 @@ def handle_notification(msg):
         task = tasks[t_i]
         rospy.loginfo('Beginning task {}'.format(task.action))
         st = State.NAVIGATE
-        dest = geometry_msgs.msg.Pose()
-        dest.position.x = task.dest[0] + ((ACTION_DIST + WALL_THICKNESS) * math.cos(task.orient))
-        dest.position.y = task.dest[1] + ((ACTION_DIST + WALL_THICKNESS) * math.sin(task.orient))
-        dest.orientation = geometry_msgs.msg.Quaternion(*quaternion_from_euler(0, 0, util.normalize_angle(task.orient - math.pi)))
-        nav_pub.publish(dest)
+        navigate(task.dest[0], task.dest[1], task.orientation)
 
 def main():
     rospy.init_node('planning')
     rospy.loginfo('planning node starting...')
 
-    global nav_pub, paint_pub
+    global move_base_client, paint_pub
 
     rate = rospy.Rate(constants.ITERATION_RATE_HZ)
+
+    rospy.loginfo('Connecting to topics...')
     notify_sub = rospy.Subscriber(constants.TOPIC_NOTIFY, std_msgs.msg.String, handle_notification)
-    nav_pub = rospy.Publisher(constants.TOPIC_NAV, geometry_msgs.msg.Pose, queue_size=10)
     paint_pub = rospy.Publisher(constants.TOPIC_PAINT, PaintTarget, queue_size=10)
+
+    rospy.loginfo('Setting for move_base client...')
+    move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    move_base_client.wait_for_server()
 
     rospy.sleep(1) # TODO: Is there a better way to wait for everything to load?
 
+    rospy.loginfo('planning node started')
+
     # Send initial navigation command
     task = tasks[t_i]
-    dest = geometry_msgs.msg.Pose()
-    dest.position.x = task.dest[0] + ((ACTION_DIST + WALL_THICKNESS) * math.cos(task.orient))
-    dest.position.y = task.dest[1] + ((ACTION_DIST + WALL_THICKNESS) * math.sin(task.orient))
-    dest.orientation = geometry_msgs.msg.Quaternion(*quaternion_from_euler(0, 0, util.normalize_angle(task.orient - math.pi)))
-    nav_pub.publish(dest)
-
-    rospy.loginfo('planning node started')
+    navigate(task.dest[0], task.dest[1], task.orientation)
 
     rospy.spin()
