31af96d8704650f564954f4f9fb7be45cc9d9276
Ryan Lankin <rlankin@unomaha.edu>
2020-02-01 22:43:01 -0600

    Fixed LoadPaintPrimitive

--- catkin_ws/src/paintbot_skiros/src/painting/primitives.py
+++ catkin_ws/src/paintbot_skiros/src/painting/primitives.py
@@ -3,6 +3,7 @@
 from skiros2_common.core.primitive import PrimitiveBase
 from descriptions import ArmToZeroDescription, LoadPaintDescription
 from actionlib_msgs.msg import GoalStatusArray
+from moveit_msgs.msg import MoveGroupActionFeedback
 import math
 import moveit_commander
 import rospy
@@ -45,18 +46,25 @@ class LoadPaintPrimitive(PrimitiveBase):
     def onInit(self):
         moveit_commander.roscpp_initialize(sys.argv)
         self.mi_cmdr = moveit_commander.MoveGroupCommander('arm')
-        rospy.Subscriber('/move_group/status', GoalStatusArray, self.feedback)
+        rospy.Subscriber('/move_group/feedback', MoveGroupActionFeedback, self.feedback)
 
     def onStart(self):
-        targets = [[_LOAD_X[i % 2], 0, 0.05, -math.pi, 0, -math.pi] for i in range(self.params['passes'].value)]
-        self.mi_cmdr.set_pose_targets(targets)
-        self.mi_cmdr.go(wait=False)
-        self.moving = True
+        self.p = -1
+        self.arm_fail = False
+        self.move = True
         return True
 
     def execute(self):
-        if self.moving:
-            return self.step('Loading paint')
+        if self.arm_fail:
+            return self.fail('Failed to load paint (unable to plan arm motion)')
+
+        if self.p < self.params['passes'].value:
+            if self.move:
+                self.mi_cmdr.set_pose_target([_LOAD_X[self.p % 2], 0, 0.05, -math.pi, 0, -math.pi])
+                self.mi_cmdr.go(wait=False)
+                self.move = False
+            return self.step('Loading paint (pass {})'.format(self.p))
+
         return self.success('Finished loading paint')
 
     def onPreempt(self):
@@ -64,7 +72,9 @@ class LoadPaintPrimitive(PrimitiveBase):
         self.mi_cmdr.clear_pose_targets()
         return self.success('Loading paint preempted')
 
-    # TODO: Need to check for when statuses change, not just existence of 0 or 1
     def feedback(self, msg):
-        # The arm is moving if any goal statuses are 0 (PENDING) or 1 (ACTIVE)
-        self.moving = any(s.status in (0, 1) for s in msg.status_list)
+        if msg.status.status == 3:
+            self.p += 1
+            self.move = True
+        elif msg.status.status == 4:
+            self.arm_fail = True
