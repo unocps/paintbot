7acddc9cc8230b1bb437c3d056a495b7742081e3
Ryan Lankin <rlankin@unomaha.edu>
2020-02-22 18:35:57 -0600

    Tweaked final output

--- tools/scene_generator/scene.py
+++ tools/scene_generator/scene.py
@@ -5,11 +5,19 @@ import wall_section
 _PAINT_ROLLER_WIDTH = 0.2286
 _WALL_SECTION_ID_START = 100
 _SCENE_OWL_TEMPLATE = """
+@prefix paintbot: <http://unomaha.edu/ontologies/paintbot#> .
+@prefix owl: <http://www.w3.org/2002/07/owl#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix xml: <http://www.w3.org/XML/1998/namespace> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@prefix skiros: <http://rvmi.aau.dk/ontologies/skiros.owl#> .
+
 skiros:Scene-0 rdf:type owl:NamedIndividual ,
         skiros:Scene ;
-    ###
-    # Tray OWL goes here
-    ###
+    #####
+    # Tray contains go here
+    #####
 {}
     skiros:DiscreteReasoner "AauSpatialReasoner"^^xsd:string ;
     skiros:FrameId "map"^^xsd:string ;
@@ -21,6 +29,10 @@ skiros:Scene-0 rdf:type owl:NamedIndividual ,
     skiros:PositionY "0.0"^^xsd:float ;
     skiros:PositionZ "0.0"^^xsd:float ;
     rdfs:label "Scene"^^xsd:string .
+
+    #####
+    # Other elements go here
+    #####
 """
 
 def gen_owl(segments):
