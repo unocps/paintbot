3c49758770aedf9d2c51cf454516170f87d535be
Ryan Lankin <rlankin@unomaha.edu>
2020-03-05 22:19:09 -0600

    NavigateToLocationPrimitive now reports deltas

--- catkin_ws/src/paintbot_skiros/src/navigation/primitives.py
+++ catkin_ws/src/paintbot_skiros/src/navigation/primitives.py
@@ -8,6 +8,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import actionlib
 import math
 import rospy
+import tf
 
 _ACTION_DIST = 0.5
 _WALL_THICKNESS = 0.05
@@ -46,7 +47,9 @@ class NavigateToLocationPrimitive(PrimitiveBase):
         goal.target_pose.pose.position.x = self.x
         goal.target_pose.pose.position.y = self.y
         goal.target_pose.pose.orientation = Quaternion(*quaternion_from_euler(0, 0, self.yaw))
-        self.mb_client.send_goal(goal, self.done_callback)
+        self.mb_client.send_goal(goal, self._done_callback)
+
+        self.tf_listener = tf.TransformListener()
 
         return True
 
@@ -54,12 +57,20 @@ class NavigateToLocationPrimitive(PrimitiveBase):
         if self.status == 1:
             return self.step('Navigating to ({}, {}) @ {}'.format(self.x, self.y, self.yaw))
         elif self.status == 3:
-            return self.success('Reached ({}, {}) @ {}'.format(self.x, self.y, self.yaw))
+            pos_delta, orient_delta = self._calc_deltas()
+            return self.success('Reached ({}, {}) @ {} [Delta: {}, {}]'.format(self.x, self.y, self.yaw, pos_delta, orient_delta))
         return self.fail('Unable to navigate to ({}, {}) @ {}'.format(self.x, self.y, self.yaw))
 
     def onPreempt(self):
         self.mb_client.cancel_goal()
         return self.success('Navigation preempted')
 
-    def done_callback(self, status, result):
+    def _calc_deltas(self):
+        try:
+            pos, orient = self.tf_listener.lookupTransform('odom', 'virtual_base', rospy.Time(0))
+            return math.sqrt((self.x - pos[0])**2 + (self.y - pos[1])**2), _normalize_angle(self.yaw - euler_from_quaternion(orient)[2])
+        except:
+            return None, None
+
+    def _done_callback(self, status, result):
         self.status = status
