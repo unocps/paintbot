8253aea051a4d0ae0b1632ce815e8a9f3893f3b5
Ryan Lankin <rlankin@unomaha.edu>
2020-03-10 00:00:11 -0500

    Added wall section entry layout

--- tools/scene_generator/scene_generator.py
+++ tools/scene_generator/scene_generator.py
@@ -27,9 +27,52 @@ class GUI:
         ws_frame = tk.LabelFrame(main_frame, text='Wall Sections')
         ws_frame.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
 
-        self.wall_sections = tk.Variable()
-        self.ws_listbox = tk.Listbox(ws_frame, listvariable=self.wall_sections)
+        self.wall_sections_var = tk.Variable()
+        self.ws_listbox = tk.Listbox(ws_frame, listvariable=self.wall_sections_var)
         self.ws_listbox.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        add_ws_frame = tk.Frame(ws_frame)
+        add_ws_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=True, padx=5, pady=(0, 5))
+
+        add_ws_start_frame = tk.Frame(add_ws_frame)
+        add_ws_start_frame.pack(fill=tk.X)
+
+        label = tk.Label(add_ws_start_frame, text='Start X:', width=6)
+        label.pack(side=tk.LEFT)
+
+        self.ws_start_x_entry = tk.Entry(add_ws_start_frame)
+        self.ws_start_x_entry.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
+
+        label = tk.Label(add_ws_start_frame, text='Start Y:', width=6)
+        label.pack(side=tk.LEFT)
+
+        self.ws_start_y_entry = tk.Entry(add_ws_start_frame)
+        self.ws_start_y_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+        add_ws_end_frame = tk.Frame(add_ws_frame)
+        add_ws_end_frame.pack(fill=tk.X)
+
+        label = tk.Label(add_ws_end_frame, text='End X:', width=6)
+        label.pack(side=tk.LEFT)
+
+        self.ws_end_x_entry = tk.Entry(add_ws_end_frame)
+        self.ws_end_x_entry.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
+
+        label = tk.Label(add_ws_end_frame, text='End Y:', width=6)
+        label.pack(side=tk.LEFT)
+
+        self.ws_end_y_entry = tk.Entry(add_ws_end_frame)
+        self.ws_end_y_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+        add_ws_paint_frame = tk.Frame(add_ws_frame)
+        add_ws_paint_frame.pack(fill=tk.X)
+
+        label = tk.Label(add_ws_paint_frame, text='Paint:', width=6)
+        label.pack(side=tk.LEFT)
+
+        self.ws_paint_var = tk.StringVar()
+        self.ws_paint_option = tk.OptionMenu(add_ws_paint_frame, self.ws_paint_var, [])
+        self.ws_paint_option.pack(fill=tk.X)
 
         gen_frame = tk.Frame(root)
         gen_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=True, padx=10, pady=(0, 10))
@@ -41,6 +84,11 @@ class GUI:
         paint = self.paint_entry.get().strip()
         if paint and paint not in self.paints.get():
             self.paint_listbox.insert(tk.END, paint)
+
+            paint_menu = self.ws_paint_option['menu']
+            paint_menu.delete(0, tk.END)
+            for p in self.paints.get():
+                paint_menu.add_command(label=p, command=lambda v=p: self.ws_paint_var.set(v))
         self.paint_entry.delete(0, tk.END)
 
     def _generate(self):
