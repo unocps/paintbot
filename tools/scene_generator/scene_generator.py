a4b38d00e98f08aefa3bb6f8a1e9823aca27c3dc
Ryan Lankin <rlankin@unomaha.edu>
2020-03-10 19:35:45 -0500

    Wall sections can now be added

--- tools/scene_generator/scene_generator.py
+++ tools/scene_generator/scene_generator.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 class GUI:
     def __init__(self, root):
+        self.ws = []
+
         self.root = root
         root.title('Paintbot Scene Generator')
 
@@ -27,52 +29,42 @@ class GUI:
         ws_frame = tk.LabelFrame(main_frame, text='Wall Sections')
         ws_frame.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
 
-        self.wall_sections_var = tk.Variable()
-        self.ws_listbox = tk.Listbox(ws_frame, listvariable=self.wall_sections_var)
+        self.ws_var = tk.Variable()
+        self.ws_listbox = tk.Listbox(ws_frame, listvariable=self.ws_var)
         self.ws_listbox.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
 
         add_ws_frame = tk.Frame(ws_frame)
         add_ws_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=True, padx=5, pady=(0, 5))
 
-        add_ws_start_frame = tk.Frame(add_ws_frame)
-        add_ws_start_frame.pack(fill=tk.X)
+        val_float = (ws_frame.register(self._val_float), '%P')
 
-        label = tk.Label(add_ws_start_frame, text='Start X:', width=6)
-        label.pack(side=tk.LEFT)
+        label = tk.Label(add_ws_frame, text='Start (x, y):')
+        label.grid(row=0, column=0, sticky='e')
 
-        self.ws_start_x_entry = tk.Entry(add_ws_start_frame)
-        self.ws_start_x_entry.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
+        self.ws_start_x_entry = tk.Entry(add_ws_frame, width=5, validate='key', validatecommand=val_float)
+        self.ws_start_x_entry.grid(row=0, column=1)
 
-        label = tk.Label(add_ws_start_frame, text='Start Y:', width=6)
-        label.pack(side=tk.LEFT)
+        self.ws_start_y_entry = tk.Entry(add_ws_frame, width=5, validate='key', validatecommand=val_float)
+        self.ws_start_y_entry.grid(row=0, column=2)
 
-        self.ws_start_y_entry = tk.Entry(add_ws_start_frame)
-        self.ws_start_y_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        label = tk.Label(add_ws_frame, text='End (x, y):')
+        label.grid(row=1, column=0, stick='e')
 
-        add_ws_end_frame = tk.Frame(add_ws_frame)
-        add_ws_end_frame.pack(fill=tk.X)
+        self.ws_end_x_entry = tk.Entry(add_ws_frame, width=5, validate='key', validatecommand=val_float)
+        self.ws_end_x_entry.grid(row=1, column=1)
 
-        label = tk.Label(add_ws_end_frame, text='End X:', width=6)
-        label.pack(side=tk.LEFT)
+        self.ws_end_y_entry = tk.Entry(add_ws_frame, width=5, validate='key', validatecommand=val_float)
+        self.ws_end_y_entry.grid(row=1, column=2)
 
-        self.ws_end_x_entry = tk.Entry(add_ws_end_frame)
-        self.ws_end_x_entry.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 5))
-
-        label = tk.Label(add_ws_end_frame, text='End Y:', width=6)
-        label.pack(side=tk.LEFT)
-
-        self.ws_end_y_entry = tk.Entry(add_ws_end_frame)
-        self.ws_end_y_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
-
-        add_ws_paint_frame = tk.Frame(add_ws_frame)
-        add_ws_paint_frame.pack(fill=tk.X)
-
-        label = tk.Label(add_ws_paint_frame, text='Paint:', width=6)
-        label.pack(side=tk.LEFT)
+        label = tk.Label(add_ws_frame, text='Paint:', width=6)
+        label.grid(row=2, column=0, sticky='e')
 
         self.ws_paint_var = tk.StringVar()
-        self.ws_paint_option = tk.OptionMenu(add_ws_paint_frame, self.ws_paint_var, [])
-        self.ws_paint_option.pack(fill=tk.X)
+        self.ws_paint_option = tk.OptionMenu(add_ws_frame, self.ws_paint_var, [])
+        self.ws_paint_option.grid(row=2, column=1, columnspan=2, sticky='ew')
+
+        add_ws_button = tk.Button(add_ws_frame, text='Add', command=self._add_ws)
+        add_ws_button.grid(row=1, column=3)
 
         gen_frame = tk.Frame(root)
         gen_frame.pack(side=tk.BOTTOM, fill=tk.X, expand=True, padx=10, pady=(0, 10))
@@ -82,17 +74,39 @@ class GUI:
 
     def _add_paint(self):
         paint = self.paint_entry.get().strip()
-        if paint and paint not in self.paints.get():
-            self.paint_listbox.insert(tk.END, paint)
+        if not paint or paint in self.paints.get():
+            return
 
-            paint_menu = self.ws_paint_option['menu']
-            paint_menu.delete(0, tk.END)
-            for p in self.paints.get():
-                paint_menu.add_command(label=p, command=lambda v=p: self.ws_paint_var.set(v))
+        self.paint_listbox.insert(tk.END, paint)
+        paint_menu = self.ws_paint_option['menu']
+        paint_menu.delete(0, tk.END)
+        for p in self.paints.get():
+            paint_menu.add_command(label=p, command=lambda v=p: self.ws_paint_var.set(v))
         self.paint_entry.delete(0, tk.END)
+
+    def _add_ws(self):
+        start_x = self.ws_start_x_entry.get().strip()
+        start_y = self.ws_start_y_entry.get().strip()
+        end_x = self.ws_end_x_entry.get().strip()
+        end_y = self.ws_end_y_entry.get().strip()
+        paint = self.ws_paint_var.get()
+        if not start_x or not start_y or not end_x or not end_y or not paint:
+            return
+
+        self.ws_listbox.insert(tk.END, '{} - ({}, {}) to ({}, {})'.format(paint, start_x, start_y, end_x, end_y))
+        self.ws.append(((start_x, start_y), (end_x, end_y), paint))
 
     def _generate(self):
         pass
+
+    def _val_float(self, s):
+        if not s:
+            return True
+        try:
+            float(s)
+            return True
+        except:
+            return False
 
 root = tk.Tk()
 gui = GUI(root)
